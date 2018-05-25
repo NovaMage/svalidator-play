d3bb01a161e527859d3a0c0fdd13bf5c94afb75d
Angel Blanco <a.blanco@magaransoft.com>
2018-05-25 16:08:26 -0400

    Adding missing TypeTag in one of the validator's methods.

--- src/main/scala/com/github/novamage/svalidator/play/binding/PlayMappingBindingValidator.scala
+++ src/main/scala/com/github/novamage/svalidator/play/binding/PlayMappingBindingValidator.scala
@@ -7,12 +7,12 @@ import scala.reflect.runtime.{universe => ru}
 
 abstract class PlayMappingBindingValidator[A: ru.TypeTag] extends MappingBindingValidator[A] {
 
-  def bindFromRequest[B](mapOp: B => A)(implicit request: Request[_]): BindingAndValidationSummary[A] = {
+  def bindFromRequest[B: ru.TypeTag](mapOp: B => A)(implicit request: Request[_]): BindingAndValidationSummary[A] = {
     val valuesMap = PlayRequestValuesMapExtractor.extractValuesMapFromRequest(request)
     super.bindAndValidate(valuesMap, mapOp)
   }
 
-  def bindLocalized[B](mapOp: B => A)(implicit messagesRequest: MessagesRequest[_]): BindingAndValidationSummary[A] = {
+  def bindLocalized[B: ru.TypeTag](mapOp: B => A)(implicit messagesRequest: MessagesRequest[_]): BindingAndValidationSummary[A] = {
     val valuesMap = PlayRequestValuesMapExtractor.extractValuesMapFromRequest(messagesRequest)
     super.bindAndValidate(valuesMap, mapOp).localize((key: String) => messagesRequest.messages(key))
   }
