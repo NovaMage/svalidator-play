0a5d2c90a577f14b2103acd133fbc03908de3446
Angel Blanco <angel.softworks@gmail.com>
2020-12-26 23:39:36 -0400

    Changed the type hierarchy of play binding validator to inherit from play mapping binding validator to avoid logic duplication

--- src/main/scala/com/github/novamage/svalidator/play/binding/PlayBindingValidatorWithData.scala
+++ src/main/scala/com/github/novamage/svalidator/play/binding/PlayBindingValidatorWithData.scala
@@ -1,30 +1,26 @@
 package com.github.novamage.svalidator.play.binding
 
-import com.github.novamage.svalidator.validation.binding.{BindingAndValidationWithData, BindingValidatorWithData}
+import com.github.novamage.svalidator.validation.binding.BindingAndValidationWithData
 import play.api.mvc.{MessagesRequest, Request}
 
 import scala.reflect.runtime.{universe => ru}
 
-abstract class PlayBindingValidatorWithData[A: ru.TypeTag, B] extends BindingValidatorWithData[A, B] {
+abstract class PlayBindingValidatorWithData[A: ru.TypeTag, B] extends PlayMappingBindingValidatorWithData[A, B] {
 
   def bindFromRequest(implicit request: Request[_]): BindingAndValidationWithData[A, B] = {
-    extractFromRequest(request, DefaultPlayRequestExtractor)
+    super.bindFromRequest((x: A) => x)
   }
 
   def bindLocalized(implicit messagesRequest: MessagesRequest[_]): BindingAndValidationWithData[A, B] = {
-    extractLocalized(messagesRequest, DefaultPlayRequestExtractor)
+    super.bindLocalized((x: A) => x)
   }
 
   def extractFromRequest(implicit request: Request[_], extractor: PlayRequestExtractor): BindingAndValidationWithData[A, B] = {
-    val valuesMap = extractor.extractValuesMapFromRequest(request)
-    val metadata = extractor.extractBindingMetadataFromRequest(request)
-    super.bindAndValidate(valuesMap, metadata)
+    super.extractFromRequest((x: A) => x)
   }
 
   def extractLocalized(implicit messagesRequest: MessagesRequest[_], extractor: PlayRequestExtractor): BindingAndValidationWithData[A, B] = {
-    val valuesMap = extractor.extractValuesMapFromRequest(messagesRequest)
-    val metadata = extractor.extractBindingMetadataFromRequest(messagesRequest)
-    super.bindAndValidate(valuesMap, metadata).localize((key: String) => messagesRequest.messages(key))
+    super.extractLocalized((x: A) => x)
   }
 
 }
